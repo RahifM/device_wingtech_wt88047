ec191f6e0d9137a6e755ac1ee990a9a2a8902fbb
ssawrav <s.sawrav@gmail.com>
2016-08-31 18:22:39 +0530

    Change Kernel Path

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,5 +1,5 @@
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
+TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
 TARGET_KERNEL_CONFIG := cyanogenmod_wt88047_defconfig
 
 BOARD_KERNEL_CMDLINE += \
