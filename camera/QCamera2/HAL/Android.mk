d7cba1324d2aa6ae5464e181080985d8de9571c2
RahifM <faizel326@gmail.com>
2017-02-11 16:54:08 +0000

    wt88047: camera: Remove Project-Path
    Signed-off-by: RahifM <faizel326@gmail.com>

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -32,10 +32,10 @@ LOCAL_CFLAGS += -DDEFAULT_DENOISE_MODE_ON
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../stack/common \
     frameworks/native/include/media/openmax \
-    $(call project-path-for,qcom-display)/libgralloc \
-    $(call project-path-for,qcom-display)/libqdutils \
-    $(call project-path-for,qcom-display)/libqservice \
-    $(call project-path-for,qcom-media)/libstagefrighthw \
+    $(TOP)/hardware/qcom/display/libgralloc \
+    $(TOP)/hardware/qcom/display/libqdutils \
+    $(TOP)/hardware/qcom/display/libqservice \
+    $(TOP)/hardware/qcom/media/libstagefrighthw \
     system/media/camera/include \
     $(LOCAL_PATH)/../../mm-image-codec/qexif \
     $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
