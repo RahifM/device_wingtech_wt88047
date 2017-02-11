c2448106c728a04ef7531003633b46c5f776347d
RahifM <faizel326@gmail.com>
2017-02-11 16:54:08 +0000

    wt88047: camera: Remove Project-Path
    Signed-off-by: RahifM <faizel326@gmail.com>

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -47,9 +47,9 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include \
         frameworks/native/include/media/openmax \
-        $(call project-path-for,qcom-display)/libgralloc \
-        $(call project-path-for,qcom-display)/libqdutils \
-        $(call project-path-for,qcom-media)/libstagefrighthw \
+        $(TOP)/hardware/qcom/display-caf-msm8916/libgralloc \
+        $(TOP)/hardware/qcom/display-caf-msm8916/libqdutils \
+        $(TOP)/hardware/qcom/media-caf-msm8916/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../../mm-image-codec/qexif \
         $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
@@ -65,7 +65,7 @@ endif
 #ifeq ($(TARGET_USE_VENDOR_CAMERA_EXT),true)
 #LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
 #else
-LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
+LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display-caf-msm8916/libgralloc
 #endif
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
