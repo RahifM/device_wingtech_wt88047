a0d45a02a51fffd09a634f37982338e3e12556cc
RahifM <faizel326@gmail.com>
2017-02-11 16:54:08 +0000

    wt88047: camera: Remove Project-Path
    Signed-off-by: RahifM <faizel326@gmail.com>

--- camera/QCamera2/HAL/test/Android.mk
+++ camera/QCamera2/HAL/test/Android.mk
@@ -40,7 +40,7 @@ LOCAL_C_INCLUDES += \
     frameworks/base/include/media \
     external/skia/include/core \
     external/skia/include/images \
-    $(call project-path-for,qcom-display)/libgralloc \
+    $(TOP)/hardware/qcom/display/msm8909/libgralloc \
     frameworks/av/include/media/stagefright \
     frameworks/native/include/media/openmax \
 
