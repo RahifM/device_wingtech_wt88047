6230906fb930e454e656e9c44f2ca8e8c1eb1abc
ssawrav <s.sawrav@gmail.com>
2017-02-26 14:00:58 +0530

    Revert "experimental: mic-gain for low in-call volume"
    This reverts commit ad3c8045817648d72886a75bf400bfd83a7bdb88.

--- device.mk
+++ device.mk
@@ -81,8 +81,9 @@ PRODUCT_COPY_FILES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence \
+    persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=false
+    persist.audio.fluence.speaker=true
 
 # Gello
 PRODUCT_PACKAGES += \
