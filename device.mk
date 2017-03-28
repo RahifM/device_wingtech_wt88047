5f8b90b085d5e43f82df0f28b0920397601988cb
Rahif <faizel326@gmail.com>
2017-03-28 11:10:17 +0530

    wt88047: updates
    Signed-off-by: Rahif <faizel326@gmail.com>

--- device.mk
+++ device.mk
@@ -24,12 +24,12 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Screen density
-PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=280
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
 
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -219,8 +219,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.msm8916 \
-    Snap
+    camera.msm8916
 
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.legacyencoder=true \
