c052c0e2f0c8fc4fa2bf1375646322295c968231
pirej <pirej30@yahoo.com>
2017-04-02 16:47:15 +0200

    Add the boringssl-compat lib for the JPEG encoder

--- device.mk
+++ device.mk
@@ -229,7 +229,8 @@ PRODUCT_PACKAGES += \
     camera.msm8916 \
     Snap \
     libmm-qcamera \
-    libshim_camera
+    libshim_camera \
+    libboringssl-compat
 
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.legacyencoder=true \
