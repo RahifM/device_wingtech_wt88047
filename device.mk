77924e7ae7066ca966943117dbc7dccfa07c17c8
ssawrav <s.sawrav@gmail.com>
2017-08-13 20:17:16 +0530

    Remove memcheck
    * No longer required

--- device.mk
+++ device.mk
@@ -189,8 +189,7 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     init.target.power.rc \
     init.qcom.usb.rc \
-    init.qcom.mem.sh \
-    memcheck
+    init.qcom.mem.sh
 
 # Sensor HAL
 PRODUCT_PACKAGES += \
