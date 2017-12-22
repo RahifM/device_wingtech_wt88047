81d0b836d234c5d80c414da66e550a57752b0822
Artem Borisov <dedsa2002@gmail.com>
2017-12-22 11:45:58 +0300

    wt88047: Dex-preopt boot and system server
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab

--- board/dexopt.mk
+++ board/dexopt.mk
@@ -1,8 +1,8 @@
 ifeq ($(HOST_OS),linux)
-    ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifneq ($(TARGET_BUILD_VARIANT),eng)
         ifeq ($(WITH_DEXPREOPT),)
             WITH_DEXPREOPT := true
-            WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
+            WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
         endif
     endif
 endif
