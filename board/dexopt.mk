c374baa387bb85c23acfaf8d051abbb46080b351
RahifM <faizel326@gmail.com>
2018-03-27 18:57:27 +0000

    wt88047: Dex gotta go!
    Signed-off-by: RahifM <faizel326@gmail.com>

--- board/dexopt.mk
+++ board/dexopt.mk
@@ -1,8 +1,8 @@
 ifeq ($(HOST_OS),linux)
     ifneq ($(TARGET_BUILD_VARIANT),eng)
         ifeq ($(WITH_DEXPREOPT),)
-            WITH_DEXPREOPT := true
-            WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+            WITH_DEXPREOPT := false
         endif
     endif
 endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
