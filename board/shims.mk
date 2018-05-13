5b024a057c960d3f5d409462958b19ae7b5ed524
MkfsSion <myychina28759@gmail.com>
2018-05-13 17:22:57 +0800

    wt88047: shim: Shim more camera blobs.
    Signed-off-by: MkfsSion <myychina28759@gmail.com>
    Signed-off-by: RahifM <faizel326@gmail.com>

--- board/shims.mk
+++ board/shims.mk
@@ -1,4 +1,10 @@
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so \
+    /system/vendor/lib/libmmcamera2_cpp_module.so|libshim_camera.so \
+    /system/vendor/lib/libmmcamera2_isp_modules.so|libshim_camera.so \
+    /system/vendor/lib/libmmcamera2_q3a_core.so|libshim_camera.so \
+    /system/vendor/lib/libmmcamera2_sensor_modules.so|libshim_camera.so \
+    /system/vendor/lib/libmmcamera2_stats_algorithm.so|libshim_camera.so \
+    /system/vendor/lib/libmmcamera2_stats_modules.so|libshim_camera.so \
     /system/vendor/lib/lib-imsvt.so|libshims_ims.so
