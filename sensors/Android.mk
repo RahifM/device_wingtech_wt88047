642660415830bba7f9ab3037c91202aa33e8fac1
RahifM <faizel326@gmail.com>
2018-05-21 22:45:28 +0530

    Fix the typo at https://github.com/RahifM/device_wingtech_wt88047/commit/ea5bbd9a3a6ca3c330021fe044969a546722fc52
    * Kudos to @MkfsSion for the fix
    
    Signed-off-by: RahifM <faizel326@gmail.com>

--- sensors/Android.mk
+++ sensors/Android.mk
@@ -27,7 +27,7 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libutils
 
-LOCAL_MODULE := sensors.$(TARGER_DEVICE)
+LOCAL_MODULE := sensors.$(TARGET_DEVICE)
 LOCAL_MODULE_TAGS := optional
 
 # Export calibration library needed dependency headers
