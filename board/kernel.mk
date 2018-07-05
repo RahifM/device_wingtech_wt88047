a7d28a88b60e0c918a8c43d27f828834572f280c
RahifM <faizel326@gmail.com>
2018-07-06 02:27:15 +0530

    Stupid me
    Signed-off-by: RahifM <faizel326@gmail.com>

--- board/kernel.mk
+++ board/kernel.mk
@@ -6,5 +6,5 @@ TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := wt88047_defconfig
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1 androidboot.selinux=permissive
 
-KERNEL_TOOLCHAIN        := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN        := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-lin-7/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-linux-gnueabi-
