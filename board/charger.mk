ed1e7dc1b237c27846ebca678282b3746c47624d
RahifM <faizel326@gmail.com>
2018-07-06 17:12:23 +0530

    wt88047: DNM: These dont do jack shit atm
    Signed-off-by: RahifM <faizel326@gmail.com>

--- board/charger.mk
+++ board/charger.mk
@@ -1,4 +1,4 @@
 # Healthd
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+#BOARD_CHARGER_ENABLE_SUSPEND := true
+#BOARD_CHARGER_DISABLE_INIT_BLANK := true
+#BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
