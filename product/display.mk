25965d0282b6ccbfc7a9f5cce86871d27fd4bd79
RahifM <faizel326@gmail.com>
2018-08-12 23:01:38 +0000

    wt88047: New DPI (295)
    Signed-off-by: RahifM <faizel326@gmail.com>

--- product/display.mk
+++ product/display.mk
@@ -4,7 +4,7 @@ TARGET_SCREEN_WIDTH := 720
 
 # Screen density
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=320
+    ro.sf.lcd_density=295
 
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
