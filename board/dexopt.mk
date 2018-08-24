f25b492f6b619439643da39f4ef642793a8287f5
RahifM <faizel326@gmail.com>
2018-08-24 16:20:25 +0530

    Revert "wt88047: Dex gotta go!"
    This reverts commit 481f665ff7f330ea9f39aa7437ec811ba41e0cb2.

--- board/dexopt.mk
+++ board/dexopt.mk
@@ -1,7 +1,7 @@
 ifeq ($(HOST_OS),linux)
     ifneq ($(TARGET_BUILD_VARIANT),eng)
         ifeq ($(WITH_DEXPREOPT),)
-            WITH_DEXPREOPT := false
+            WITH_DEXPREOPT := true
         endif
     endif
 endif
