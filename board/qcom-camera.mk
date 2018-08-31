aec5cfcafb9b8bdc917e4e5e7a68edbcf533ba18
Danny Baumann <dannybaumann@web.de>
2018-08-31 13:35:20 +0200

    wt88047: Mark camera-related processes as API level 22.
    Our camera blobs trigger some checks for broken behavior (destroyed
    mutex usage, invalid pthread usage) present in newer API levels.
    
    Change-Id: I8705437e0a5c6d80b86748bff559ce1e8a7bf716
    Signed-off-by: RahifM <faizel326@gmail.com>

--- board/qcom-camera.mk
+++ board/qcom-camera.mk
@@ -2,5 +2,8 @@
 BOARD_CAMERA_SENSORS := ov2680_5987fhq ov8865_q8v18a ov2680_skuhf
 BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
 USE_DEVICE_SPECIFIC_CAMERA := true
-#TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE := true
+TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
+    /system/bin/cameraserver=22 \
+    /system/bin/mediaserver=22 \
+    /system/vendor/bin/mm-qcamera-daemon=22
