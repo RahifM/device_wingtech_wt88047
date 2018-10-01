11f1e61975639cbde1f3321253ae23db77716839
RahifM <faizel326@gmail.com>
2018-10-01 07:50:58 +0000

    wt88047: Citrus-CAF Bringup
    Signed-off-by: RahifM <faizel326@gmail.com>

--- citrus_wt88047.mk
+++ citrus_wt88047.mk
@@ -15,8 +15,8 @@
 
 $(call inherit-product, device/wingtech/wt88047/full_wt88047.mk)
 
-# Grab APNs
-PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+# Inherit some common stuff.
+$(call inherit-product, vendor/citrus/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
@@ -24,7 +24,7 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8916
 # Assert
 TARGET_OTA_ASSERT_DEVICE := HM2014811,HM2014812,HM2014813,HM2014814,HM2014815,HM2014816,HM2014817,HM2014818,HM2014819,HM2014820,HM2014821,HM2014112,wt88047,wt86047
 
-PRODUCT_NAME := aosp_wt88047
+PRODUCT_NAME := citrus_wt88047
 BOARD_VENDOR := wingtech
 PRODUCT_DEVICE := wt88047
 
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo aosp_wt88047-eng
-add_lunch_combo aosp_wt88047-userdebug
