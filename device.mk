cbc8f4f53ed3838fef63e32964b596506c06d65d
RahifM <faizel326@gmail.com>
2018-11-01 15:50:16 +0530

    temp
    * Kanged from https://android.googlesource.com/device/google/marlin/+/ae6d0886ec5f3b1c05e81252d39cf1eaa3dfc110%5E%21/#F0
    
    Signed-off-by: RahifM <faizel326@gmail.com>

--- device.mk
+++ device.mk
@@ -25,3 +25,7 @@ include $(LOCAL_PATH)/product/*.mk
 
 # Inherit the rest from msm8916-common
 $(call inherit-product, device/cyanogen/msm8916-common/msm8916.mk)
+
+# Privileged permissions whitelist
+PRODUCT_COPY_FILES += \
+    device/wingtech/wt88047/permissions/privapp-permissions-omni_wt88047.xml:system/etc/permissions/privapp-permissions-omni_wt88047.xml
