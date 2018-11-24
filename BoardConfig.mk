ad45180d527584b93ce2e2fa77702119fa66b2ee
Josh Choo <dev@joshchoo.com>
2018-11-24 03:41:37 +0000

    wt88047: Set DISABLE_EAP_PROXY to break wpa_supplicant dependency on qmi
    Without this flag, wpa_supplicant fails to build because we do not have
    sources for these proprietary libs: libqmi_cci, libqmiservices, libidl.
    
    Reference:
    https://source.codeaurora.org/quic/la/platform/external/wpa_supplicant_8/commit/?h=cfd8fe3d57bcf0976dddb0d82ec04fcd2dbc5ebc
    
    Change-Id: Id161eadd9e5327768d1f41fb621d4422aeaa27ec
    Signed-off-by: Josh Choo <dev@joshchoo.com>
    Signed-off-by: RahifM <faizel326@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,3 +94,6 @@ PRODUCT_SHIPPING_API_LEVEL := 19
 
 # inherit from the proprietary version
 include vendor/wingtech64/wt88047_64/BoardConfigVendor.mk
+
+# Disable qmi EAP-SIM security
+DISABLE_EAP_PROXY := true
