a04cc9ad9b5299ba3ad582cdb0bd771921c9e5e7
RahifM <faizel326@gmail.com>
2018-12-31 22:03:04 +0530

    wt88047: May the force be with us
    Signed-off-by: RahifM <faizel326@gmail.com>

--- board/kernel.mk
+++ board/kernel.mk
@@ -4,4 +4,4 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := wt88047_defconfig
-BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
