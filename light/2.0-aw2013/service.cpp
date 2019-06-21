7a81a42eb4ae8fa429682dd3ba8bdb04c1ebac94
RahifM <faizel326@gmail.com>
2019-06-21 14:52:08 +0530

    wt88047_64: light: Make changes so that it wont conflict with 32bit trees
    Signed-off-by: RahifM <faizel326@gmail.com>

--- light/2.0-aw2013/service.cpp
+++ light/2.0-aw2013/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.light@2.0-service.aw2013"
+#define LOG_TAG "android.hardware.light@2.0-service.aw2013-wt88047_64"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
