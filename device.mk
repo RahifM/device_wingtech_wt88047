LineageOS/android_device_wingtech_wt88047

wt88047: system-properties: Migrate from system.prop to system_prop.mk
* The build system auto-including system.prop will be deprecated in the
  near future, so migrate to setting all system propeties with a makefile.

Change-Id: Ia640d4a8adf2eaa68c801aba084fd01063af1463
Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
Signed-off-by: Nicholas Lim <nicknitewolf@lineageos.org>

device.mk
@@ -117,6 +117,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
 
+# System Properties
+$(call inherit-product, device/wingtech/wt88047/system_prop.mk)
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine.conf

RahifM/device_wingtech_wt88047

Signed-off-by: RahifM <faizel326@gmail.com>

device.mk
@@ -119,6 +119,9 @@ PRODUCT_PACKAGES += \
     libshims_camera \
     libshims_ims
 
+# System Properties
+$(call inherit-product, device/wingtech/wt88047_64/system_prop.mk)
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine.conf