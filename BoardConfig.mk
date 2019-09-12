c1f95f1a7adbfb8b31a94f1173fc1425ea56b3a7
Nicholas Lim <nicknitewolf@lineageos.org>
2019-09-12 14:59:07 +0800

    hax
    Change-Id: I5a4085e110b970283e185adf968a9a87423bbfa7
    Signed-off-by: RahifM <faizel326@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,8 +82,8 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 BOARD_NO_SECURE_DISCARD := true
 
 # SELinux
-BOARD_SEPOLICY_DIRS += \
-    $(DEVICE_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    $(DEVICE_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS += \
