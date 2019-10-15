48d7967b41aa834f266de1347eede63346d17ab8
RahifM <faizel326@gmail.com>
2019-10-15 16:59:18 +0530

    Revert "wt88047: May the force be with us"
    This reverts commit 8303ed44ded02956acc3893ea66dd88e7c4750bc.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,7 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := wt88047_defconfig
-BOARD_KERNEL_CMDLINE += phy-msm-usb.floated_charger_enable=1
+BOARD_KERNEL_CMDLINE += phy-msm-usb.floated_charger_enable=1 androidboot.selinux=permissive
 
 # Power
 TARGET_HAS_NO_POWER_STATS := true
