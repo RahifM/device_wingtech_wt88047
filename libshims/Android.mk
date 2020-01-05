70e9fb97a6dbaef03bf748e9fd9bad570ee42aec
RahifM <faizel326@gmail.com>
2020-01-05 17:18:33 +0530

    Revert "Revert "wt88047_64: libshims: Add shims for IMS""
    This reverts commit 8d0474e5139415d6bad2f315bcac1d7440dee9d2.

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -25,3 +25,76 @@ LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    boringssl/p_dec.c \
+    boringssl/p_open.c \
+    boringssl/cipher.c \
+    boringssl/e_des.c \
+    boringssl/cleanup.c \
+    boringssl/ctrl.c
+
+LOCAL_CFLAGS += -std=c99
+LOCAL_C_INCLUDES := boringssl
+LOCAL_SHARED_LIBRARIES := libcrypto
+LOCAL_MODULE := libshims_boringssl
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    camera/Camera.cpp \
+    camera/CameraMetadata.cpp \
+    camera/CaptureResult.cpp \
+    camera/CameraParameters2.cpp \
+    camera/ICamera.cpp \
+    camera/ICameraClient.cpp \
+    camera/ICameraService.cpp \
+    camera/ICameraServiceListener.cpp \
+    camera/ICameraServiceProxy.cpp \
+    camera/ICameraRecordingProxy.cpp \
+    camera/ICameraRecordingProxyListener.cpp \
+    camera/camera2/ICameraDeviceUser.cpp \
+    camera/camera2/ICameraDeviceCallbacks.cpp \
+    camera/camera2/CaptureRequest.cpp \
+    camera/camera2/OutputConfiguration.cpp \
+    camera/CameraBase.cpp \
+    camera/CameraUtils.cpp \
+    camera/VendorTagDescriptor.cpp \
+    camera/CameraParameters.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils \
+    liblog \
+    libbinder \
+    libhardware \
+    libui \
+    libgui \
+    libcamera_metadata
+
+LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH)/camera/include \
+    system/media/camera/include \
+    system/media/private/camera/include
+
+LOCAL_MODULE := libshims_camera
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := ims/MediaBuffer.c
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_MODULE := libshims_ims
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
