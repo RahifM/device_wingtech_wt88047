db847b0910c496cd79a2273bb2142fbdfb99b132
RahifM <faizel326@gmail.com>
2020-03-03 21:50:57 +0530

    wt88047: BoardConfig: Fix header build
    Signed-off-by: RahifM <faizel326@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := lineageos_wt88047_defconfig
 BOARD_KERNEL_CMDLINE += phy-msm-usb.floated_charger_enable=1 androidboot.selinux=permissive
+TARGET_COMPILE_WITH_MSM_KERNEL := true
 
 # Power
 TARGET_HAS_NO_POWER_STATS := true
