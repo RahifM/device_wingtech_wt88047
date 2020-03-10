a967d165c34bdec31f468b308523a5b9845add2c
RahifM <faizel326@gmail.com>
2020-03-10 23:32:02 +0530

    WIP: add prebuilt kernel
     * placebo kernel from pie build out
    
    Signed-off-by: RahifM <faizel326@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,12 +67,12 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_wt88047
 
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
-BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_KERNEL_SEPARATED_DT := true
-TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
-TARGET_KERNEL_CONFIG := lineageos_wt88047_defconfig
+#BOARD_KERNEL_IMAGE_NAME := Image
+#BOARD_KERNEL_SEPARATED_DT := true
+#TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
+#TARGET_KERNEL_CONFIG := lineageos_wt88047_defconfig
 BOARD_KERNEL_CMDLINE += phy-msm-usb.floated_charger_enable=1 androidboot.selinux=permissive
-TARGET_COMPILE_WITH_MSM_KERNEL := true
+#TARGET_COMPILE_WITH_MSM_KERNEL := true
 
 # Power
 TARGET_HAS_NO_POWER_STATS := true
