d0c1bd3fb438474eac1865fd956301ff1dba43bb
RahifM <faizel326@gmail.com>
2021-03-28 02:37:37 +0530

    wt88047: ABC
    Signed-off-by: RahifM <faizel326@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_wt88047.mk
+    $(LOCAL_DIR)/aosp_wt88047.mk \
 
-COMMON_LUNCH_CHOICES := \
-    aosp_wt88047-user \
-    aosp_wt88047-userdebug \
-    aosp_wt88047-eng
+#COMMON_LUNCH_CHOICES := \
+ #   aosp_wt88047-user \
+  #  aosp_wt88047-userdebug \
+   # aosp_wt88047-eng
